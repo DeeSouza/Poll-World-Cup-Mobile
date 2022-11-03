7253a24f3afeb21f6b1301cca4c3c115c3563107
Diego Souza <dee-souza@hotmail.com>
2022-11-03 12:26:22 -0300

    create structure project

---
+++ src/Home/index.tsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import { Text, SafeAreaView } from "react-native";
+
+export function Home() {
+  return (
+    <SafeAreaView>
+      <Text>Olá</Text>
+    </SafeAreaView>
+  );
+}

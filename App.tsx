7baf30133d1cf2e77b0dbb2b0e7c6626304fe658
Diego Souza <dee-souza@hotmail.com>
2022-11-04 12:49:29 -0300

    create components and screens

--- App.tsx
+++ App.tsx
@@ -7,10 +7,14 @@ import {
 } from "@expo-google-fonts/roboto";
 
 import { AuthContextProvider } from "./src/context/AuthContext";
+import { Loading } from "./src/components/Loading";
+
+import { SignIn } from "./src/screens/SignIn";
+import { New } from "./src/screens/New";
+import { Find } from "./src/screens/Find";
+import { Pools } from "./src/screens/Pools";
 
 import { THEME } from "./src/styles/theme";
-import { Loading } from "./src/components/Loading";
-import { SignIn } from "./src/screens/SignIn";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -27,7 +31,7 @@ export default function App() {
           backgroundColor="transparent"
           translucent
         />
-        {fontsLoaded ? <SignIn /> : <Loading />}
+        {fontsLoaded ? <Pools /> : <Loading />}
       </AuthContextProvider>
     </NativeBaseProvider>
   );
